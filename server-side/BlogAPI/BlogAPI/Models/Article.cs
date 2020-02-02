9d755d9d57a468f940f6212056538dc4c09600f1
Valeriy Kokorev <kokorev.valery@gmail.com>
2020-02-02 19:21:33 +0700

    Реализовал API для статей

---
+++ server-side/BlogAPI/BlogAPI/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlogAPI.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime PubDate { get; set; }
+
+        public string Text { get; set; }
+    }
+}
